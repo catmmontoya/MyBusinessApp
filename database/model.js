9b83b7cd17f8fd27d848c57d8bb20b48d1398eff
Caitlin Baldini <cat@listoglobal.com>
2024-08-23 12:38:35 -0600

    item table added to db

--- database/model.js
+++ database/model.js
@@ -1,5 +1,38 @@
-// import { DataTypes, Model } from "sequelize";
+import { DataTypes, Model } from "sequelize";
 import connectToDB from "./db.js";
-// import util from "util";
+import util from "util";
 
 export const db = await connectToDB(`postgresql:///catmontoya`);
+
+class Item extends Model {
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
+}
+
+Item.init(
+  {
+    itemId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    picture: {
+      type: DataTypes.TEXT,
+    },
+    itemName: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    modelName: "item",
+    sequelize: db,
+  }
+);
+
+export { Item };
